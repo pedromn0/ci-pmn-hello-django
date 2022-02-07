6f57e26088f71a5ba52e3477823927512fa20b3e
pedromn0 <muzzioneri@gmail.com>
2022-02-07 12:06:39 +0000

    Fix environments variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get('SECRET_KEY', django-insecure-l!oj$2bdug&gsi#g@g2g_aj(36%33fji_x9cf$k+4(qxujcbrw')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l!oj$2bdug&gsi#g@g2g_aj(36%33fji_x9cf$k+4(qxujcbrw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
